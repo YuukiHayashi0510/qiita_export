0d93fd2b1d8651aae7dd62022fc3fa37a1b8a3ad
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2025-03-07 15:07:35 +0900

    fix: 不要なモデルのコード

--- models/comment.go
+++ models/comment.go
@@ -1,10 +1,6 @@
 package models
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
 	"time"
 )
 
@@ -18,39 +14,4 @@ type Comment struct {
 	UpdatedAt      time.Time       `json:"updated_at"`
 	User           User            `json:"user"`
 	EmojiReactions []EmojiReaction `json:"reactions"` // 絵文字リアクション, 同一エンドポイントでは取得できない
-}
-
-// コメントモデルのIDを利用して、絵文字リアクションをAPI経由で取得する
-// GET /api/v2/comments/:comment_id/reactions にリクエストを送信し、格納する
-func (c *Comment) RequestEmojiReactions(domain, token string) ([]EmojiReaction, error) {
-	requestUrl := fmt.Sprintf("https://%s/api/v2/comments/%s/reactions", domain, c.ID)
-	authHeaderToken := fmt.Sprintf("Bearer %s", token)
-
-	req, err := http.NewRequest(http.MethodGet, requestUrl, nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Authorization", authHeaderToken)
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("failed to get emoji reactions: %s", res.Status)
-	}
-
-	// 絵文字リアクション情報を格納する
-	emojiReactions := make([]EmojiReaction, 0)
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-	if err := json.Unmarshal(body, &emojiReactions); err != nil {
-		return nil, err
-	}
-
-	return emojiReactions, nil
 }
