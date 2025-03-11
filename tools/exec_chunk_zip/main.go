40fccd87390a77dba432d59a038de2cacba1aaa8
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2025-03-11 10:59:27 +0900

    fix: exec_chunk_zip

--- tools/exec_chunk_zip/main.go
+++ tools/exec_chunk_zip/main.go
@@ -11,11 +11,14 @@ import (
 )
 
 const (
-	chunkSize = 100
+	defaultChunkSize = 100
 )
 
 func main() {
 	targetDir := flag.String("dir", "output", "default value is 'output'")
+	targetZip := flag.Int("zip", 0, "specify zip number to process (0 for all)")
+	chunkSize := flag.Int("chunk", defaultChunkSize, "number of directories per zip file")
+	excludeIDs := flag.String("exclude", "", "comma-separated list of IDs to exclude")
 	flag.Parse()
 
 	// ディレクトリ内のエントリを取得
@@ -36,23 +39,27 @@ func main() {
 	splits := strings.Split(*targetDir, "/")
 	baseName := splits[len(splits)-1]
 
+	filteredDirs := filterDirs(dirs, strings.Split(*excludeIDs, ",")...)
+
 	// チャンク数の計算
-	numChunks := (len(dirs) + chunkSize - 1) / chunkSize
+	numChunks := (len(filteredDirs) + *chunkSize - 1) / *chunkSize
 
 	// チャンク毎にZip化
-	for i := 0; i < numChunks; i++ {
-		start := i * chunkSize
-		end := start + chunkSize
-		if end > len(dirs) {
-			end = len(dirs)
+	for i := range numChunks {
+		// 指定されたZIP番号と一致しない場合はスキップ
+		if *targetZip != 0 && *targetZip != i+1 {
+			continue
 		}
+
+		start := i * *chunkSize
+		end := min(start+*chunkSize, len(filteredDirs))
 
 		// ZIPファイル名
 		zipName := fmt.Sprintf("%s_%d.zip", baseName, i+1)
 
-		// ZIP化 - 対象のディレクトリを直接追加
-		// コマンドの引数を用意
-		args := append([]string{"-r", zipName}, dirs[start:end]...)
+		// ZIP化 - このチャンクのディレクトリだけを追加
+		args := append([]string{"-r", zipName}, filteredDirs[start:end]...)
+		args = append(args, "-x", "*_metadata.json")
 
 		cmd := exec.Command("zip", args...)
 		if err := cmd.Run(); err != nil {
@@ -62,4 +69,25 @@ func main() {
 
 		fmt.Printf("Created %s (directories %d-%d of %d)\n", zipName, start+1, end, len(dirs))
 	}
+}
+
+func filterDirs(dirs []string, excludeIDs ...string) []string {
+	filtered := make([]string, 0, len(dirs)-len(excludeIDs))
+
+	for _, v := range dirs {
+		isContainedExcludeID := false
+		for _, eid := range excludeIDs {
+			if strings.Contains(v, eid) {
+				isContainedExcludeID = true
+				break
+			}
+		}
+
+		if !isContainedExcludeID {
+			fmt.Println(filepath.Base(v))
+			filtered = append(filtered, v)
+		}
+	}
+
+	return filtered
 }
